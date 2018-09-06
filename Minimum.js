3de5ec5836d029f4757a3b2d68ebc3ae3d4fdbc2
Tatiana Scott <hello@tatianascott.com>
2018-09-06 19:50:54 -0400

    solution

---
+++ Minimum.js
@@ -0,0 +1,21 @@
+/*
+Minimum
+The previous chapter introduced the standard function Math.min that returns its smallest argument. 
+We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+
+// Your code here.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+
+*/
+
+function minimum (firstNum, secondNum){
+    if (firstNum < secondNum) {
+        return firstNum;
+    } else {
+        return secondNum;
+    }
+}
