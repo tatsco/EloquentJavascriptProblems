9c8f819ef50bc2482e30fadfc51191d2ab50cb14
Tatiana Scott <hello@tatianascott.com>
2018-09-11 16:40:31 -0400

    solution update

--- TheSumOfARange.js
+++ TheSumOfARange.js
@@ -32,7 +32,9 @@ function range (start, end) {
   
   
   // As a bonus assignment, modify your range function to take an optional third argument that indicates
-  // the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+  // the “step” value used when building the array. If no step is given, the elements go up by increments 
+  //of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9].
+  // Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
   
   function range (start, end, n) {
     let arrOfRanges = [];
