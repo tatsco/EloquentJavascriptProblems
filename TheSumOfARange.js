3a37a1b8194b50f4411256ff10d10652902428ab
Tatiana Scott <hello@tatianascott.com>
2018-09-11 16:39:42 -0400

    updated solution

--- TheSumOfARange.js
+++ TheSumOfARange.js
@@ -1,9 +1,11 @@
 // The sum of a range
-// The introduction of this book alluded to the following as a nice way to compute the sum of a range of numbers:
+// The introduction of this book alluded to the following as a nice way to compute the sum of a
+// range of numbers:
 
 // console.log(sum(range(1, 10)));
 
-// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+// Write a range function that takes two arguments, start and end, and returns an array containing
+// all the numbers from start up to (and including) end.
 
 function range (start, end) {
     let arrOfRanges = [];
@@ -16,7 +18,8 @@ function range (start, end) {
   console.log(range(1, 10));
   // → [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
   
-  // Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+  // Next, write a sum function that takes an array of numbers and returns the sum of these numbers. 
+  //Run the example program and see whether it does indeed return 55.
   
   function sum (arrOfNums) {
     return arrOfNums.reduce(function(total, elem){
@@ -28,7 +31,8 @@ function range (start, end) {
   // // → 55
   
   
-  // As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+  // As a bonus assignment, modify your range function to take an optional third argument that indicates
+  // the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
   
   function range (start, end, n) {
     let arrOfRanges = [];
